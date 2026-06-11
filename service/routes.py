f1d1b89fd9d8444f7f6f59b62a6dad54e379c4c3
Bhawana234 <Bhawana.mh444@gmail.com>
2026-06-11 14:18:42 -0400

    Add CI build workflow

--- service/routes.py
+++ service/routes.py
@@ -4,7 +4,7 @@ Account Service
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
-from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
@@ -42,7 +42,6 @@ def index():
 def create_accounts():
     """
     Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
     """
     app.logger.info("Request to create an Account")
     check_content_type("application/json")
@@ -51,12 +50,12 @@ def create_accounts():
     account.create()
     message = account.serialize()
     # Uncomment once get_accounts has been implemented
-    #location_url = url_for("get_accounts", account_id=account.id, _external=True)
+    # location_url=url_for("get_accounts",account_id=account.id,_external=True)
     location_url = "/"  # Remove once get_accounts has been implemented
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
-    
+
     ####################################################################
     # LIST ALL ACCOUNTS
     ######################################################################
@@ -75,9 +74,6 @@ def create_accounts():
         return jsonify(account_list), status.HTTP_200_OK
 
 
-
-
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -91,16 +87,12 @@ def get_account(account_id):
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.",
+        )
 
     return jsonify(account.serialize()), status.HTTP_200_OK
-
-
-    
-    
-
-
-
 
     ######################################################################
     # UPDATE AN EXISTING ACCOUNT
@@ -115,14 +107,15 @@ def get_account(account_id):
 
         account = Account.find(account_id)
         if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+            abort(
+                status.HTTP_404_NOT_FOUND,
+                f"Account with id [{account_id}] could not be found.",
+            )
 
         account.deserialize(request.get_json())
         account.update()
 
         return account.serialize(), status.HTTP_200_OK
-
-
 
     ######################################################################
     # DELETE AN ACCOUNT
@@ -131,7 +124,6 @@ def get_account(account_id):
     def delete_accounts(account_id):
         """
         Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
         """
         app.logger.info("Request to delete an Account with id: %s", account_id)
 
@@ -140,7 +132,6 @@ def get_account(account_id):
             account.delete()
 
         return "", status.HTTP_204_NO_CONTENT
-
 
 
 ######################################################################
@@ -158,18 +149,3 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
-
-    def test_get_account_not_found(self):
-        """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_method_not_allowed(self):
-        """It should not allow an illegal method call"""
-        resp = self.client.delete(BASE_URL)
-        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
-       
-
-
